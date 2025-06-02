8471488bb07222a2aa5ff99febf5ece5afac3257
eliasescalante <eliasw.escalante@gmail.com>
2025-06-02 12:56:32 -0300

    agrego hud e item de nivel mas map del nivel

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -11,3 +11,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/level.tscn")
