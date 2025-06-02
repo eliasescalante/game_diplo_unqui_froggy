bed9a34d38f4272d55d176a5d3b63ec492b82db0
eliasescalante <eliasw.escalante@gmail.com>
2025-06-02 13:54:53 -0300

    todas las escenas con los nodos completos

---
+++ src/scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+@onready var anim := $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	var input_vector := Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	velocity = input_vector * SPEED
+	move_and_slide()
+
+	# Animaciones
+	if input_vector != Vector2.ZERO:
+		anim.play("jump")
+	else:
+		anim.play("idle")
